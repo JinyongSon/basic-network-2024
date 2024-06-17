cd38fcff5fb427d3e952c3acc3e5717484481f87
JinyongSon <wlsdydths@naver.com>
2024-06-17 17:24:07 +0900

    5일차

--- day05/echo_stderv.c
+++ day05/echo_stderv.c
@@ -12,12 +12,14 @@ int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
     char message[BUF_SIZE];
-    int str_len , i;
+    int str_len, i;
 
-    struct sockaddr_in serv_adr, clnt_adr;
+    struct sockaddr_in serv_adr;
+    struct sockaddr_in clnt_adr;
     socklen_t clnt_adr_sz;
-
-    if (argc != 2) {
+    FILE * readfp;
+    FILE * writefp;
+    if(argc != 2) {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
@@ -37,8 +39,6 @@ int main(int argc, char *argv[])
     if(listen(serv_sock, 5) == -1)
         error_handling("listen() error");
 
-    clnt_adr_sz = sizeof(clnt_adr);
-
     for(i=0; i<5; i++)
     {
         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
@@ -46,11 +46,17 @@ int main(int argc, char *argv[])
             error_handling("accept() error");
         else
             printf("Connected client %d \n", i+1);
-
-        while((str_len = read(clnt_sock, message, BUF_SIZE)) != 0)
-            write(clnt_sock, message, str_len)
-
-        close(clnt_sock);
+        
+        readfp = fdopen(clnt_sock, "r");
+        writefp = fdopen(clnt_sock, "w");
+        while(!feof(readfp))
+        {
+            fgets(message, BUF_SIZE, readfp);
+            fputs(message, writefp);
+            fflush(writefp);
+        }
+        fclose(readfp);
+        fclose(writefp);
     }
     close(serv_sock);
     return 0;
